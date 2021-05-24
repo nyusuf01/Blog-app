3b6059e60bc8cb4aaf44836c0ca228ab7219f4f6
Cameron Brilliant <cameron@Camerons-MacBook-Air.local>
2021-05-24 16:39:21 -0400

    Components/Screens

---
+++ client/src/screens/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Layout from "../../components/Layout/Layout"
+import Post from "../../components/Post/Post"
+
+function Home() {
+  return (
+    <Layout>
+      <Post />
+    </Layout>
+  )
+}
+
+export default Home
